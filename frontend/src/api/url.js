c944ef80dfa22f6d7aaf7c1713aa6936869546c7
Tapash Ranjan Nandi <tapashnandi777@gmail.com>
2025-11-21 23:07:18 +0530

    commited frontend

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BACKEND_URL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/noteapp/"
+    baseURL: "http://a96632fd6bf694a3cb80937733f78843-1967690016.ap-south-1.elb.amazonaws.com:4001/api/v1/noteapp/"
 })
 
 export default BACKEND_URL
